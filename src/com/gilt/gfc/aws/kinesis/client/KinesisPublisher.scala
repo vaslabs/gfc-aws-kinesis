77926c6c8fa9eca8dfc1224ccef82e61f2815389
Kevin Schultz <kschultz@gilt.com>
2015-07-07 15:41:27 -0400

    collect all classes into gfc package

--- src/com/gilt/gfc/aws/kinesis/client/KinesisPublisher.scala
+++ src/com/gilt/gfc/aws/kinesis/client/KinesisPublisher.scala
@@ -1,4 +1,4 @@
-package com.amazonaws.kinesis
+package com.gilt.gfc.aws.kinesis.client
 
 import java.nio.ByteBuffer
 import java.util.concurrent._
--- src/com/gilt/gfc/aws/kinesis/client/KinesisRecord.scala
+++ src/com/gilt/gfc/aws/kinesis/client/KinesisRecord.scala
@@ -1,4 +1,4 @@
-package com.amazonaws.kinesis
+package com.gilt.gfc.aws.kinesis.client
 
 /**
  * Simplified view of AWS SDK's kinesis record, just a couple of things we care about.
