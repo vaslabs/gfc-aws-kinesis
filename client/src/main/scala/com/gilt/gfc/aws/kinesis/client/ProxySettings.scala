95c5e0124206dd2194b49f4241ed7591b1eb501c
Vasilis Nicolaou <vasilisnicolaou@rentalcars.com>
2017-03-08 17:42:56 +0000

    Better to use a case class

--- client/src/main/scala/com/gilt/gfc/aws/kinesis/client/ProxySettings.scala
+++ client/src/main/scala/com/gilt/gfc/aws/kinesis/client/ProxySettings.scala
@@ -5,11 +5,7 @@ import com.amazonaws.ClientConfiguration
 /**
   * Created by nicolaouv on 08/03/17.
   */
-trait ProxySettings {
-  val host: String
-  val port: Int
-  val username: Option[String] = None
-  val password: Option[String] = None
+case class ProxySettings(host: String, port: Int, username: Option[String] = None, password: Option[String] = None) {
   private[client] def configureClientConfiguration(conf: ClientConfiguration): Unit = {
     conf.setProxyHost(this.host)
     conf.setProxyPort(this.port)
